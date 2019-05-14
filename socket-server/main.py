94c9f5c55ad38fc10951c346b9570c969dcb51b6
Emilien Gauthier <biglouch@hotmail.fr>
2019-05-14 16:31:54 +0200

    add ip choice on server start

--- socket-server/main.py
+++ socket-server/main.py
@@ -3,6 +3,8 @@ from aiohttp import web
 from typing import List
 import netifaces as ni
 
+from PyInquirer import prompt
+
 sio = socketio.AsyncServer(async_mode='aiohttp')
 app = web.Application()
 sio.attach(app)
@@ -28,14 +30,57 @@ async def disconnect(sid):
     print(nodes)
     await sio.emit('nodes', nodes)
 
+
 async def index(request):
     return web.Response(text='bonjour')
+
 
 app.router.add_get('/', index)
 
 
+def get_ip_addresses() -> List[str]:
+    ip_addresses: List[str] = []
+    for int in ni.interfaces():
+        net: List[dict] = ni.ifaddresses(int).get(ni.AF_INET)
+        if net and net[0].get('addr') != '127.0.0.1':
+            ip_addresses.append(net[0].get('addr'))
+    return ip_addresses
+
+
+def choose_ip_address(ip_addresses: List[str]) -> str:
+    for index, addr in enumerate(ip_addresses):
+        print('{}) {}'.format(index + 1, addr))
+
+    num = -1
+    while num < 1 or num > len(ip_addresses):
+        num = int(input('Choose your wifi card ip: '))
+    return ip_addresses[num - 1]
+
 if __name__ == '__main__':
-    ip = ni.ifaddresses(ni.interfaces()[-1])[ni.AF_INET][0]['addr']
-    print('To deploy server (Ubuntu 18):\n- sudo ufw enable && sudo ufw allow 8000\n')
-    print('Server ip to connect :', ip)
-    web.run_app(app, host=ip, port=8000)
+    ip_addresses = get_ip_addresses()
+    ip_server = ''
+
+    try:
+        questions: List[dict] = [
+            {
+                'type': 'list',
+                'name': 'ip',
+                'message': 'Choose your wifi card ip',
+                'choices': ip_addresses
+            }
+        ]
+
+        answers = prompt(questions)
+        ip_server = answers.get('ip')
+    except:
+        ip_server = choose_ip_address(ip_addresses)
+
+    print(ip_server)
+
+
+    if ip_server:
+        print('To deploy server (Ubuntu 18):\n- sudo ufw enable && sudo ufw allow 8000\n')
+        print('Server ip to connect :', ip_server)
+        web.run_app(app, host=ip_server, port=8000)
+    else:
+        print('No ip provided')
