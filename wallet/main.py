367de4e11a820e415c7ce90373e45a2c537991a1
Emilien Gauthier <biglouch@hotmail.fr>
2019-06-04 09:20:06 +0200

    delete unused code

--- wallet/main.py
+++ wallet/main.py
@@ -1,9 +1,5 @@
 from gui.GUI import GUI
-import asyncio
 
 if __name__ == '__main__':
     fenetre = GUI()
-    # loop = asyncio.get_event_loop()
-    # mainloop = loop.run_in_executor(None, fenetre.mainloop)
-    # loop.run_until_complete(mainloop)
     fenetre.mainloop()
