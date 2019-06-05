460ef0cd3a13f012b209b5bff1251fc74bc6b5eb
Emilien Gauthier <biglouch@hotmail.fr>
2019-06-05 12:12:29 +0200

    add installer for windows + executable on linux

---
+++ wallet/setup.py
@@ -0,0 +1,35 @@
+import sys
+from cx_Freeze import setup, Executable
+
+import os.path
+
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+print(sys.platform)
+
+options = {'build_exe': {
+    # 'path': sys.path,
+    'packages': ['asyncio', 'requests', 'engineio.async_drivers.threading'],
+    'includes': ['idna.idnadata']
+}
+}
+exe = Executable('main.py')
+
+if sys.platform == 'win32':
+    os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+    os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+    options['build_exe']['include_files'] = [
+        os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+        os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll')
+    ]
+    exe.targetName = 'Xatome.exe'
+
+elif sys.platform == 'linux':
+    exe.targetName = 'Xatome'
+
+setup(name='XatomeCoin',
+      version='0.1',
+      description='Xatome Coin Miner',
+      options=options,
+      executables=[exe]
+      )
